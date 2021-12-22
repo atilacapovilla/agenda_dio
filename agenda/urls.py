f3758d8cd4dc570f03ee57123aba2e7e0eadfcb1
atilacapovilla <capovillaatila@gmail.com>
2021-12-22 19:04:28 -0300

    Teste de token

--- agenda/urls.py
+++ agenda/urls.py
@@ -9,6 +9,7 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', views.index),
+    # outra forma de redirect
     path('', RedirectView.as_view(url='/agenda/')),
     path('agenda/', views.lista_eventos)
 ]
