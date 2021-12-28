b11d5869ee349db87aa97cebb6c0efa2ee44489d
atilacapovilla <capovillaatila@gmail.com>
2021-12-28 16:18:39 -0300

    Filtros por data, configurado timezone, respose Http404 e Json

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
@@ -9,6 +10,7 @@ class Evento(models.Model):
 
     class Meta:
         db_table = 'evento'
+        ordering = ('data_evento',)
         
     def __str__(self):
         return self.titulo
@@ -18,3 +20,9 @@ class Evento(models.Model):
 
     def get_data_input_evento(self):
         return self.data_evento.strftime('%Y-%m-%dT%H:%M')
+
+    def get_evento_atrasado(self):
+        if self.data_evento < datetime.now():
+            return True
+        else:
+            return False
